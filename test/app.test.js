d3049b10fe04daa717b0595868b2b8256ee75f4a
Mak-Di <makarov.dmitriy@gmail.com>
2021-10-18 11:24:28 +0300

    Update app.test.js

--- test/app.test.js
+++ test/app.test.js
@@ -3,3 +3,7 @@ const app = require('./../app');
 test('adds 1 + 2 to equal 3', () => {
     expect(app.sum(1, 2)).toBe(3);
 });
+
+test('subs 9 - 7 to equal 2', () => {
+    expect(app.sum(9, 7)).toBe(2);
+});
